0c4b9408d2def03f15e403586aba0af6ef3a5f6b
Mathias Picker <mattis123@live.no>
2022-02-12 13:03:57 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+import {fade} from 'svelte/transition'
+
+let visible = false;
+</script>
+
+<button on:click={() => visible = !visible}>Toggle visibility</button>
+
+{#if visible}
+    <div transition:fade>Hello!</div>
+{/if}
